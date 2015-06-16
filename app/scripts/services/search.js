ee962b226447a082c3a11e753dd7b5cedb2e2849
elruffo <leonardo.ruffin@gmail.com>
2015-06-16 16:16:20 +0200

    Update search url

--- app/scripts/services/search.js
+++ app/scripts/services/search.js
@@ -4,7 +4,7 @@
 'use strict';
 
 angular.module('brewlarderclientApp').factory('search', ['$http', function($http) {
-  return $http.get('http://localhost:8000/en/search?type=beer&q=orval&withBreweries=Y', {withCredentials: true})
+  return $http.get('https://brewlarderapi.herokuapp.com/en/search?type=beer&q=orval&withBreweries=Y', {withCredentials: true})
     .success(function(data) {
       return data;
     })
